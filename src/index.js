cf2e666dff40e7fd2767cb663a3a9a3305973a1f
janlue <zaj.dev.learning@gmail.com>
2020-07-03 15:56:29 +0200

    Chart realisiert

--- src/index.js
+++ src/index.js
@@ -11,111 +11,157 @@ class SeriesItem {
     * @param {string} year
     * @param {string} value
     */
-    constructor(year, value) {
 
+    year;
+    value; 
+    constructor(year, value) {
+        this.year = year;
+        this.value = value;
     }
 }
 
-class Chart {    
+class Chart {        
 
-    base = {
-        //stats 
-        minX: 1880,
-        minY: -1.0,
-        maxX: 2010,
-        maxY: 1.0, 
-
-        //visualization
-        padding: 10,
-        tickSize: 10,
-        axisColor: "#000000",
-        pointRadius: 5, 
-        xTicks: 1,
-        yTicks: 1
+    years = {
+        start: '',
+        end: '', 
+        decades: []
     }
 
-    calculated = {
-        rangeX: this.base.maxX - this.base.minY,
-        rangeY: this.base.maxY - this.base.minY,
-        numXTicks: Math.round(this.base.maxX - this.base.minY / this.base.xTicks),
-        numYTicks: Math.round(this.base.maxY - this.base.minY / this.base.yTicks),
-        x: Math.round(this.base.maxX - this.base.minY / this.base.xTicks) * this.base.xTicks,
-        y: Math.round(this.base.maxY - this.base.minY / this.base.yTicks) * this.base.yTicks, 
-        width: '',
-        height: '', 
-        scaleX: '',
-        scaleY: ''
+    xAxis = {
+        startX: 50,
+        startY: 280,
+        endX: 1550,
+        endY: 280,  
     }
 
+    yAxis = {
+        startX: 50,
+        startY: 550,
+        endX: 50,
+        endY: 50
+    }
 
     /**
     * Creates a new Chart object
     * @param {object} canvas
     */
-    constructor(canvas) {        
-        canvas.width = 1200;
+    constructor(canvas, items) {        
+        canvas.width = 1600;
         canvas.height = 600;
-
         let ctx = canvas.getContext("2d");
-        
 
-        this.calculated.width = canvas.width / this.calculated.rangeX;
-        this.calculated.height = canvas.height / this.calculated.rangeY;
-        this.calculated.scaleX = this.calculated.width / this.calculated.rangeX;
-        this.calculated.scaleY = this.calculated.height / this.calculated.rangeY;
-        
-        //this.drawAxis(ctx);
-
-        // Stroked triangle
-        ctx.beginPath();
-        ctx.moveTo(40, 560);
-        ctx.lineTo(1160, 560);    
-        ctx.closePath();
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(40, 560);
-        ctx.lineTo(40, 40);
-        ctx.closePath();
-        ctx.stroke();
+        this.setYears(items);
+        this.drawChart(ctx);  
+        this.draw(items, ctx);              
     }
 
-    drawAxis(ctx){
-        ctx.save();
-        ctx.beginPath();
-        ctx.moveTo(this.calculated.x, this.calculated.y + this.calculated.height);
-        ctx.strokeStyle = this.base.axisColor;
-        ctx.lineWidth = 2;
-        ctx.stroke();
+    setYears(items){
+        this.years.start = items[items.length - 1].year;
+        this.years.end = items[0].year;
+
+        for(let i = 0; i < items.length; i++){
+            let year = items[i].year;            
+            if(!this.years.decades.includes(year) && (year % 10 == 0)){
+                this.years.decades.push(year);
+            } 
+        }
     }
 
+    drawChart(ctx){
+        //draw Axis 
+        // y - Axis
+        ctx.beginPath();
+        ctx.moveTo(this.yAxis.startX, this.yAxis.startY);
+        ctx.lineTo(this.yAxis.endX, this.yAxis.endY);
+        ctx.closePath();
+        ctx.stroke();
+        
+        //draw axis labeling & orientation lines   
+        // x - Axis
+        let xRange = (this.xAxis.endX - this.xAxis.startX) / this.years.decades.length;
+        let yearPosition = this.xAxis.startY + 290;
+        
+        for(let i = this.xAxis.startX, j = 1; i < (this.xAxis.endX - this.xAxis.startX), j <= this.years.decades.length; i = i + xRange, j++){
+            ctx.fillText(this.years.decades[this.years.decades.length - j], i, yearPosition);
+
+            ctx.lineWidth = 0.5;
+            ctx.beginPath();
+            ctx.strokeStyle = "#D8D8D8";
+            ctx.moveTo(i + xRange, this.yAxis.startY);
+            ctx.lineTo(i + xRange, this.yAxis.endY);
+            ctx.closePath();
+            ctx.stroke();                       
+        }
+
+        // y - Axis
+        let yRange = (this.yAxis.startY - this.yAxis.endY) / 4;
+        let values = ['-1.00', '-0.50', '0.00', '0.50', '1.00'];
+
+        for(let i = this.yAxis.startY, j = 0; i > this.yAxis.endY, j < values.length; i = i - yRange, j++){
+            ctx.fillText(values[j], this.yAxis.startX - 32, i);
+
+            // check for middle line to make it thicker
+            if(j == 1){
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.strokeStyle = "#000000";
+                ctx.moveTo(this.yAxis.startX, i - yRange);
+                ctx.lineTo(this.xAxis.endX, i - yRange);
+                ctx.closePath();
+                ctx.stroke();    
+            } else {
+                ctx.lineWidth = 0.5;
+                ctx.beginPath();
+                ctx.strokeStyle = "#D8D8D8";
+                ctx.moveTo(this.yAxis.startX, i - yRange);
+                ctx.lineTo(this.xAxis.endX, i - yRange);
+                ctx.closePath();
+                ctx.stroke();    
+            }                
+        }                  
+    }
 
     /**
     * Draws series on the canvas
     * @param {SeriesItem[]} series
     */
-    draw(series) {
+    draw(series, ctx) {
+        let yAxisRange = this.yAxis.startY - this.yAxis.endY;
+        let xAxisRange = this.xAxis.endX - this.xAxis.startX;
+
+        console.log(series);
+        
+        for(let i = 0; i < series.length; i++){
+            ctx.fillRect(this.mapXCoordinates(series[i].year, xAxisRange), this.mapYCoordinates(series[i].value, yAxisRange), 3, 3);
+        }
     }
+
+    mapXCoordinates(year, range){        
+        return (year - this.years.start) * (range - 50) / (this.years.end - this.years.start) + 50;
+    }
+    
+    mapYCoordinates(value, range){        
+        return (value - (1.0)) * (range - 50) / (-1.0 - (1.0)) + 50;
+    }
+
 }
 
 class Application {
+    canvas = document.getElementById("canvas");    
 
-    canvas = document.getElementById("canvas");
-    items = [];    
-
-    init(){  
+    init(){             
         fetch("../data/daten.json")
              .then(res => res.json())
-             .then(data => data.map(item => {
-                this.items.push(item);
-                })
-            );  
-            
-            console.log(this.items);
+             .then(data => {
+                let items = [data.length]; 
+                for(let i = 0; i < data.length; i++){
+                    items[i] = new SeriesItem(data[i].Year, data[i].Mean);
+                }
+                new Chart(canvas, items);
+             }            
+        );        
     }
-
-    chart = new Chart(canvas);
-    
 }
 
 
